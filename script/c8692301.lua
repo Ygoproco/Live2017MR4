5b1bf00a0c919cb0c8ab1225cdeb896bd9017c2c
edo9300 <edoardo762@gmail.com>
2017-08-20 20:01:15 +0200

    Those should be all the changed scripts (#1)

---
+++ script/c8692301.lua
@@ -0,0 +1,6 @@
+--ジェムナイト・ジルコニア
+function c8692301.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcMix(c,true,true,aux.FilterBoolFunction(Card.IsFusionSetCard,0x1047),aux.FilterBoolFunction(Card.IsRace,RACE_ROCK))
+end
