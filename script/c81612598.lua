53e56166a6b9afb217977d2f135c7e80162c0316
edo9300 <edoardo762@gmail.com>
2017-08-20 19:40:01 +0200

    Those should be all the changed scripts

---
+++ script/c81612598.lua
@@ -0,0 +1,6 @@
+--メタルフォーゼ・アダマンテ
+function c81612598.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcMix(c,true,true,aux.FilterBoolFunction(Card.IsFusionSetCard,0xe1),aux.FilterBoolFunction(Card.IsAttackBelow,2500))
+end
