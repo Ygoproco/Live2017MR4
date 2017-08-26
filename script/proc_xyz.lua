4cea11cf3e68b38847fb44685e45223f2195c95c
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-08-26 14:19:09 +0800

    Xyz Proc Update to match TFSP + LOTD

--- script/proc_xyz.lua
+++ script/proc_xyz.lua
@@ -601,23 +601,17 @@ function Auxiliary.XyzCondition2(alterf,op)
 				else
 					mg=Duel.GetFieldGroup(tp,LOCATION_MZONE,LOCATION_MZONE)
 				end
-				return (not min or min<=1) and (not op or op(e,tp,0)) and mg:IsExists(Auxiliary.XyzAlterFilter,1,nil,alterf,c,e,tp,op)
+				return (not min or min<=1) and mg:IsExists(Auxiliary.XyzAlterFilter,1,nil,alterf,c,e,tp,op)
 			end
 end
 function Auxiliary.XyzTarget2(alterf,op)
 	return	function(e,tp,eg,ep,ev,re,r,rp,chk,c,og,min,max)
 				local cancel=not og and Duel.GetCurrentChain()<=0
-				if cancel then
-					Auxiliary.ProcCancellable=true
-				end
-				if op then
-					local ok,apply=op(e,tp,1)
-					if not ok then return false end
-					if apply then cancel=false end
-				end
+				Auxiliary.ProcCancellable=cancel
 				if og and not min then
 					og:KeepAlive()
 					e:SetLabelObject(og)
+					if op then op(e,tp,1,og:GetFirst()) end
 					return true
 				else
 					local mg=nil
@@ -626,15 +620,14 @@ function Auxiliary.XyzTarget2(alterf,op)
 					else
 						mg=Duel.GetFieldGroup(tp,LOCATION_MZONE,LOCATION_MZONE)
 					end
-					local minct=cancel and 0 or 1
 					Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-					local g=mg:FilterSelect(tp,Auxiliary.XyzAlterFilter,minct,1,nil,alterf,c,e,tp,op)
-					if g:GetCount()>0 then
-						if op then op(e,tp,2,g:GetFirst()) end
-						g:KeepAlive()
-						e:SetLabelObject(g)
-						return true
-					else return false end
+					local oc=mg:Filter(Auxiliary.XyzAlterFilter,nil,alterf,c,e,tp,op):SelectUnselect(Group.CreateGroup(),tp,cancel,cancel)
+					if not oc then return false end
+					local ok=true
+					if op then ok=op(e,tp,1,oc) end
+					if not ok then return false end
+					e:SetLabelObject(oc)
+					return true
 				end
 			end
 end	
