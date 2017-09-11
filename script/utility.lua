e93c33d3cdfe8c1019ac617dae445e0e4b1ef7f9
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-09-11 15:09:22 -0300

    revert bool function to previous stage
    nil checker was added in core

--- script/utility.lua
+++ script/utility.lua
@@ -199,13 +199,7 @@ end
 --provisory adjust
 function Auxiliary.TargetBoolFunction(f,a,b,c)
 	return	function(effect,target)
-				if b and c then
-					return f(target,a,b,c)
-				elseif b then
-					return f(target,a,b)
-				else
-					return f(target,a)
-				end
+				return f(target,a,b,c)
 			end
 end
 function Auxiliary.FilterEqualFunction(f,value,a,b,c)
@@ -219,16 +213,9 @@ function Auxiliary.FilterBoolFunctionEx(f,value)
 				return f(target,value,scard,sumtype,tp)
 			end
 end
---provisory fix (same reason as above)
 function Auxiliary.FilterBoolFunction(f,a,b,c)
 	return	function(target)
-				if b and c then
-					return f(target,a,b,c)
-				elseif b then
-					return f(target,a,b)
-				else
-					return f(target,a)
-				end
+				return f(target,a,b,c)
 			end
 end
 Auxiliary.ProcCancellable=false
