5a8aa0d4f273208a5b058466604821237963f324
EerieCode <eeriecode@gmail.com>
2017-10-18 09:34:27 +0200

    Fixed "Mythical Beast Jackal"

--- script/c91182675.lua
+++ script/c91182675.lua
@@ -80,7 +80,7 @@ function c91182675.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(c,REASON_COST)
 end
 function c91182675.spfilter(c,e,tp)
-	return (c:IsSetCard(0x10c) or c:IsCode(55424270)) and not c:IsCode(91182675)
+	return (c:IsSetCard(0x10d) or c:IsCode(55424270)) and not c:IsCode(91182675)
 		and c:IsType(TYPE_EFFECT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c91182675.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
